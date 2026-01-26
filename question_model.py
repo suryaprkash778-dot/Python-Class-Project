f2834b3e12eb65d47c48f7c4f55eeeaff987d178
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-01-26 17:08:41 +0530

    Refactor QuizBrain class and add Question class

--- question_model.py
+++ question_model.py
@@ -1,25 +1,4 @@
-class QuizBrain:
-    def __init__(self,q_list):
-        self.question_number = 0
-        self.question_list = q_list
-
-
-
-
-    def next_question(self,q_num,q_list):
-        ans = True
-        while ans:
-            user_ans = input(f"Q.{q_num + 1}: {q_list[q_num].text}. (True/False)?:").title()
-            if user_ans == q_list[q_num].answer:
-                print(f"You got it right!\nThe correct answer was: {q_list[q_num].answer}."
-                      f"\nYour current score is: {q_num + 1}/{q_num + 1}")
-                print("\n")
-                q_num += 1
-                if q_num == 12:
-                    print("You've completed the quiz")
-                    ans = False
-
-            else:
-                print(f"That's wrong.\nThe correct answer was: {q_list[q_num].answer}"
-                      f"\nYour final score is: {q_num}/{q_num + 1}")
-                ans = False
+class Question:
+    def __init__(self,text,answer):
+        self.text = text
+        self.answer = answer
