0d5304c7119040ea340f44c9bdac36b34f4718ae
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-02-12 13:41:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
-
 for question in question_data:
-    q_text = question["text"]
-    q_answer = question["answer"]
-    ques = Question(q_text,q_answer)
-    question_bank.append(ques)
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
 
 quiz = QuizBrain(question_bank)
-quiz.next_question(0,question_bank)
+quiz_ui = QuizInterface(quiz)
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
